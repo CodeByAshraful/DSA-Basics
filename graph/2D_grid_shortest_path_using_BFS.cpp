bea15d3e9cc0ab53f3dfc89a6cbd29e57c3de514
Ashraful Khan <aponkhanbd884@gmail.com>
2025-12-28 00:38:46 +0600

    Add files via upload
    uploaded in 2D grid how we can reach destination with shortest path using BFS

---
+++ graph/2D_grid_shortest_path_using_BFS.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+char grid[105][105];
+bool vis[105][105];
+int level[105][105];
+int n,m;
+vector<pair<int,int>>d={{-1,0},{1,0},{0,-1},{0,1}};
+bool valid(int i,int j)
+{
+    if(i<0 || i>=n || j<0 || j>=m)
+    {
+        return false;
+    }
+    return true;
+}
+
+void bfs(int si,int sj)
+{
+    queue<pair<int,int>>q;
+    q.push({si,sj});
+    vis[si][sj]=true;
+    level[si][sj]=0;
+    while(!q.empty())
+    {
+        pair<int,int>par = q.front();
+        q.pop();
+        int par_i = par.first;
+        int par_j = par.second;
+     
+        for(int i=0; i<4; i++)
+        {
+            int ci = par_i + d[i].first;
+            int cj = par_j + d[i].second;
+
+            if(valid(ci,cj) && !vis[ci][cj])
+            {
+                q.push({ci,cj});
+                vis[ci][cj]=true;
+                level[ci][cj]=level[par_i][par_j]+1;
+            }
+        }
+
+    }
+
+}
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            cin>>grid[i][j];
+        }
+    }
+    memset(vis,false,sizeof(vis));
+    memset(level,-1,sizeof(level));
+    int si,sj,di,dj; //d=destination
+    cin>>si>>sj>>di>>dj;
+    bfs(si,sj);
+    cout<<"Shortest path : "<< level[di][dj]<<endl;
+    
+    return 0;
+}
+/*
+sample input : 
+3 4
+. . . .
+. . . .
+. . . .
+1 2
+2 0
+sample output : 
+Shortest path : 33
+
+*/
